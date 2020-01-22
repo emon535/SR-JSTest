904773029ef54f189e3a90cb40125ca57e8e34dc
Foysal Ahmed Emon <emon535@gmail.com>
2020-01-23 00:18:17 +0600

    Mobile number validation added

--- AddressBook/src/routes/AddressBook.js
+++ AddressBook/src/routes/AddressBook.js
@@ -1,26 +1,40 @@
 let express = require("express");
 let route = express.Router();
 let PersonModel = require("./../models/person.model");
+const { check, validationResult } = require("express-validator");
 
 route.get("/status", (req, res) => {
   res.send("Server is up and Running");
 });
 
-route.post("/contact", (req, res) => {
-  if (!req.body) return res.status(400).send("Request body is missing");
-  let model = new PersonModel(req.body);
-  model
-    .save()
-    .then(doc => {
-      console.log("Then", doc);
-      res.status(201).send(doc);
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    });
+route.post(
+  "/contact",
+  [check("mobile").isMobilePhone(), check("mobile").isLength(11)],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array(),
+        message: "Must be Bangladeshi mobile number ea: 01520100000."
+      });
+    }
 
-  console.log("Data Saved");
-});
+    if (!req.body) return res.status(400).send("Request body is missing");
+
+    let model = new PersonModel(req.body);
+    model
+      .save()
+      .then(doc => {
+        console.log("Then", doc);
+        res.status(201).send(doc);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+
+    console.log("Data Saved");
+  }
+);
 
 route.get("/contacts", (req, res) => {
   if (!req.body) return res.status(400).send("Request body is missing");
