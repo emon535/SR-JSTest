00840730405e2aa6209e5fbe0d4e79738bc9b4d1
Foysal Ahmed Emon <emon535@gmail.com>
2020-01-22 20:38:42 +0600

    reset

--- AddressBook/src/database.js
+++ AddressBook/src/database.js
@@ -3,10 +3,10 @@ let mongoose = require("mongoose");
 const server = "ds211709.mlab.com";
 const database = "emon-mlab";
 const user = "emon535";
-const password = "535Emon//";
+const password = "lokomono";
 const db = mongoose.connection;
 
-const dbPath = "mongodb://emon535:535Emon//@ds211709.mlab.com:11709/emon-mlab";
+const dbPath = "mongodb://emon535:lokomono//@ds211709.mlab.com:11709/emon-mlab";
 mongoose.connect(dbPath);
 
 db.on("error", () => {
