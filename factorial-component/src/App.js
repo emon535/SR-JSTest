3d6e534653b7c39525564888a9dd43a5bb2a6c57
Foysal Ahmed Emon <emon535@gmail.com>
2020-01-22 16:05:32 +0600

    Factorial function moved outside App- MaxCall issue.

--- factorial-component/src/App.js
+++ factorial-component/src/App.js
@@ -7,19 +7,16 @@ export default function App() {
   const [status, setStatus] = useState("");
 
   // Calculated the factorial number.
-  function getFactorial(n) {
-    return n ? n * getFactorial(n - 1) : 1;
-  }
 
   // handled the button click event
   const handleSubmit = event => {
     let factorialValue = 0;
     event.preventDefault();
     if (input > 100) {
-      setStatus("The input is too big. [ Range 0-100 ]");
+      setStatus("The input is too big [ Range 0-100 ].");
       setFactorial(0);
     } else if (input < 0) {
-      setStatus("The input is too small. [ Range 0-100 ]");
+      setStatus("The input is too small [ Range 0-100 ].");
       setFactorial(0);
     } else {
       setStatus("");
@@ -48,4 +45,12 @@ export default function App() {
       </p>
     </div>
   );
+}
+
+function getFactorial(input) {
+  if (input == 0) {
+    return 1;
+  } else {
+    return input * getFactorial(input - 1);
+  }
 }
